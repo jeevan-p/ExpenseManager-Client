c9c45eeedbddd644560b606ea8bd68249522ebb6
Jeevan Paul Puthukka <jeevan.puthukka@wipro.com>
2019-01-04 17:06:05 +0530

    G1:Removed unwanted image logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import InitialPage from './views/InitialPage.js';
 import LoginPage from './views/LoginPage.js';
